a4741116a7912b00c5a0f181f58d196d5789f18b
Gabriel B) <weaselssh@gmail.com>
2025-06-20 16:52:28 -0600

    intento de arreglo

--- Song/src/song/Song.java
+++ Song/src/song/Song.java
@@ -3,46 +3,52 @@ package song;
 import javax.swing.ImageIcon;
 
 public class Song {
-        
-        private int codigo;
-        private String nombre;
-        private double precio;
-        private int sumaEstrellas = 0;
-        private int totalReviews = 0;
-        
-        ImageIcon imagenDisco = null;
-        
-        public Song(int codigo, String nombre, double precio, ImageIcon imagenDisco){
-            this.codigo = codigo;
-            this.nombre = nombre;
-            this.precio = precio;
-            this.imagenDisco = imagenDisco;
+
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private int sumaEstrellas = 0;
+    private int totalReviews = 0;
+
+    ImageIcon imagenDisco = null;
+
+    public Song(int codigo, String nombre, double precio, ImageIcon imagenDisco) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagenDisco = imagenDisco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public ImageIcon getDisco() {
+        return imagenDisco;
+    }
+
+    public void addStars(int stars) {
+        if (stars >= 0 && stars <= 5) {
+            sumaEstrellas += stars;
+            totalReviews++;
         }
-        
-        public int getCodigo(){
-            return codigo;
-        }
-        
-        public String getNombre(){
-            return nombre;
-        }
-        
-        public double getPrecio(){
-            return precio;
-        }
-        
-        public ImageIcon getDisco(){
-            return imagenDisco;
-        }
-        
-        public void addStars(int stars){
-            if(stars >= 0 && stars <= 5){
-                sumaEstrellas += stars;
-                totalReviews++;
-            }
-        }
-        
-        public int sonRating(){
-            return sumaEstrellas / totalReviews;
-        }    
+    }
+
+    public int sonRating() {
+        return sumaEstrellas / totalReviews;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
 }
