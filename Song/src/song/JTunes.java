cb092870d4216ffa6ea985eeeef1e64115ee06a9
Gabriel B) <weaselssh@gmail.com>
2025-06-20 14:04:48 -0600

    creación de JTunes (carcasa)

---
+++ Song/src/song/JTunes.java
@@ -0,0 +1,23 @@
+package song;
+
+public class JTunes {
+    private Song canciones[];
+    
+    public cancion (Song canciones[]) {
+    this.canciones = canciones;
+    }
+    
+    public boolean addSong (int codigo, String nombre, double precio, ImageIcon imagenDisco) {
+        for (Song cancion : canciones) {
+            if (cancion != null && codigo != ) {
+                
+            }
+        }
+    }
+    
+    public Song searchSong(int codigo) {
+        return null;
+    }
+    
+    
+}
