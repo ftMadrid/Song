f849b8604ce273dc3e21bb44c07beac3f42f537e
ftMadrid <fernandomadridcv04@gmail.com>
2025-06-20 14:10:53 -0600

    Update Song.java

--- Song/src/song/Song.java
+++ Song/src/song/Song.java
@@ -1,21 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package song;
 
-/**
- *
- * @author ricardomadrid
- */
-public class Song {
+import javax.swing.ImageIcon;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hola");
-    }
+public class Song {
+        
+        private int codigo;
+        private String nombre;
+        private double precio;
+        private int sumaEstrellas = 0;
+        private int totalReviews = 0;
+        
+        ImageIcon imagenDisco = null;
+        
+        public Song(int codigo, String nombre, double precio, ImageIcon imagenDisco){
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.imagenDisco = imagenDisco;
+        }
+        
+        public int getCodigo(){
+            return codigo;
+        }
+        
+        public String getNombre(){
+            return nombre;
+        }
+        
+        public double getPrecio(){
+            return precio;
+        }
+        
+        public ImageIcon getDisco(){
+            return imagenDisco;
+        }
+        
+        public void addStars(int stars){
+            if(stars >= 0 && stars <= 5){
+                sumaEstrellas += stars;
+                totalReviews++;
+            }
+        }
+        
+        public int sonRating(){
+            return sumaEstrellas / totalReviews;
+        }
+        
+        
+        
+        
+        
+        
+       
     
 }
